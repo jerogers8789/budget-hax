7d8dea6bbba84a88776f385af344fdaba88dc26f
JER.8789 <je.rogers8789@gmail.com>
2021-06-05 19:54:38 -0500

    completed indexdb file and tweaked the last bit of service worker file

--- public/indexdb.js
+++ public/indexdb.js
@@ -1,1 +1,49 @@
 let db;
+
+const req = indexDB.open('budget', 1);
+
+req.onupgradeneeded = function (e) {
+    const db = e.target.result;
+    db.createObjectStore('pending', {autoIncrement: true});
+};
+req.onsuccess = function(e) {
+    db = e.target.result;
+    if (navigator.onLine) {
+        checkDatabase();
+    }
+};
+req.onerror = function (e) {
+    console.log(e.target.errorCode);
+};
+
+const saveRecord = function(record) {
+    let transaction = db.transaction(['pending'], 'readwrite');
+    let store = transaction.objectStore('pending');
+    store.add(record);
+}
+
+const checkDatabase = function() {
+    let transaction = db.transaction(['pending'], 'readwrite');
+    let store = transaction.objectStore('pending');
+    let getAll = store.getAll();
+
+    getAll.onsuccess = function() {
+        if (getAll.result.length > 0) {
+            fetch('/api/transaction/bulk', {
+                method: 'POST',
+                body: JSON.stringify(getAll.result),
+                headers: {
+                    Accept: 'application/json, text/plain, */*',
+                    'content-type': 'application/json'
+                }
+            })
+            .then(res => res.json())
+            .then(() => {
+                let transaction = db.transaction(['pending'], 'readwrite');
+                let store = transaction.objectStore('pending');
+                store.clear();
+            });
+        }
+    };
+}
+window.addEventListener('online', checkDatabase);
--- public/service-worker.js
+++ public/service-worker.js
@@ -1,22 +1,53 @@
+
+  const CACHE_NAME = "static-cache-v2";
+  const DATA_CACHE_NAME = "data-cache-v1";
+  const FILES_TO_CACHE = [
+    '/',
+    '/index.html',
+    '/db.js',
+    '/index.js',
+    '/style.css',
+  ];
+
 self.addEventListener('install', e => {
     e.waitUntil(
         caches.open('static').then(cache =>{
-            return cache.addAll([
-                './',
-                'index.html',
-                './styles.css',
-                './icons/icon-192x192.png',
-                './icons/icon-512x512.png'
-            ]);
+            return cache.addAll(FILES_TO_CACHE);
         })
     );
-    console.log('install');
+    console.log('installed');
     self.skipWaiting();
 });
+
+e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) =>
+    cache.add('./icons'))
+)
+e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE))
+)
 self.addEventListener('fetch', e =>{
-    e.respondWith(
-        caches.match(e.request).then(request => {
-            return response || fetch (e.request);
-        })
+    if (e.request.url.includes('/api')) {
+        e.respondWith(
+            caches.open(DATA_CACHE_NAME).then (cache => {
+                return fetch(e.request)
+                .then(response => {
+                    if(response.status === 200) {
+                        cache.put(e.request.url, res.clone());
+                    }
+                    return res;
+                })
+                .catch(err => {
+                    return cache.match(e.request);
+                });
+            }).catch(err => console.log(err))
+        );
+        return;
+    }
+    e.respondWith(caches.open(CACHE_NAME).then(cache => {
+        return cach.match(e.request).then(res => {
+            return res || fetch(e.request);
+        });
+    })
     );
 });
